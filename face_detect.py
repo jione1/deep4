e7b156193c1d7dd325932a0fc284161bc72176d5
Soongja <david0801@naver.com>
2018-07-04 15:06:43 +0900

    jisooya

--- face_detect.py
+++ face_detect.py
@@ -2,8 +2,11 @@ import cv2
 import os
 import glob
 
+image_path = './aoa'
+save_path = './aoa_save'
+
 # Get user supplied values
-file_names = glob.glob('./aoa/*.png') + glob.glob('./aoa/*.jpg')
+file_names = glob.glob(image_path + '/*.png') + glob.glob(image_path + '/*.jpg')
 print(file_names)
 
 for f in file_names:
@@ -22,17 +25,17 @@ for f in file_names:
         gray,
         scaleFactor=1.2,
         minNeighbors=5,
-        minSize=(16, 16),
+        minSize=(10, 10),
         flags = cv2.CASCADE_SCALE_IMAGE
     )
 
     print(f)
-    print("Found {0} faces!".format(len(faces)))
+    print("Found {0} face(s)!".format(len(faces)))
     print(faces)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-    cv2.imwrite('./aoa/box.jpg', image)
+    cv2.imwrite('%s/%s' % (save_path, f.split('\\')[1]) , image)
     #else:
         #pass
